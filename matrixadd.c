16a26fd97f70befbd84f82caa12487b8b6771303
Clayton Kristiansen <kristiansenc@DESKTOP-3DV10QD.localdomain>
2022-10-12 18:22:24 -0500

    read in files

---
+++ matrixadd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *inA;
+    FILE *inB;
+    inA = fscanf("inA.txt", "r");
+    inB = fscanf("inB.txt", "r");
+    
+    return 0;
+}
